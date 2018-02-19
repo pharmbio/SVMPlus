6380e99d434d592853c8fe30e9045b4547b0176a
Niharika <niharika.gauraha@farmbio.uu.se>
2018-02-19 08:18:25 +0100

    grid search for SVM plus

--- sigDesc.py
+++ sigDesc.py
@@ -123,110 +123,120 @@ def gridSearchWithCV(fileName):
     y_prob = clf.fit(X, y).predict_proba(X_test)
     fpr, tpr, thresholds = roc_curve(y_test, y_prob[:, 1])
     finalAUC = auc(fpr, tpr)
-    print("param C = %f, gamma = %f, AUC = %f \n" % (C, gamma, finalAUC))
+    #print("param C = %f, gamma = %f, AUC = %f \n" % (C, gamma, finalAUC))
     ofile.write("param C = %f, gamma = %f, AUC = %f \n" % (C, gamma, finalAUC))
 
     ofile.close()
 
 
 
-#run SVM+ for sign descriptor files
-def compareSVMAndSVMPlus(fileName1, fileName2, fingDescFile):
-    X_train, X_test, y_train, y_test, indices_train,indices_test = \
-        loadDataset(fileName1, split = True, returnIndices = True)
+# run SVM+ for sign descriptor files
+def gridSearchSVMPlus(MACCSFile, topoFile, fingDescFile):
+    paramC = [.001, .01, .1, 1, 100, 1000]
+    paramGamma = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, .1]
+    X, X_test, y, y_test, indices_train,indices_test = \
+        loadDataset(MACCSFile, split = True, returnIndices = True)
 
-    XStar, yStar = loadDataset(fileName2, split=False)
+    XStar, yStar = loadDataset(topoFile)
 
     XStar_train = XStar[indices_train]
     XStar_test = XStar[indices_test]
     yStar_train = yStar[indices_train]
     yStar_test = yStar[indices_test]
 
-    print(X_train.shape)
-    print(XStar_train.shape)
+    #print(X_train.shape)
+    #print(XStar_train.shape)
 
-    if (y_train == yStar_train).all() and (y_test == yStar_test).all() :
+    if (y == yStar_train).all() and (y_test == yStar_test).all() :
         print("same split for both the files")
     else:
         sys.exit("different split for X and XStar")
 
     XFD, yFD = csr.readCSRFile(fingDescFile)
-
-    XFD_train = XFD[indices_train]
     XFD_test = XFD[indices_test]
-    yFD_train = yFD[indices_train]
+    XFD = XFD[indices_train]
     yFD_test = yFD[indices_test]
+    yFD = yFD[indices_train]
 
-    if (y_train == yFD_train).all() and (y_test == yFD_test).all():
+    if (y == yFD).all() and (y_test == yFD_test).all():
         print("same split for both the files")
     else:
         sys.exit("different split for X and XFD")
 
-    # compute prediction accuracy using SVM for file1
-    #svm_clf = svmPlus.svmPlusOpt(X_train, y_train, XStar=None, C=10, kernel="linear",
-    #                             kernelParam=None)  # standard SVM
-    #y_predict = svmPlus.predict(X_test, svm_clf)
-    #correct = np.sum(y_predict == y_test)
-    #print("Prediction accuracy using SVM for ", fileName1)
-    #print("%d out of %d predictions correct" % (correct, len(y_predict)))
-    '''
-    clf = svm.SVC(gamma=.00001, C=100.)
-    clf.fit(X_train, y_train)
-    #print(len(clf.support_), "support vectors")
-    y_predict = clf.predict(X_test[:1000])
-    correct = np.sum(y_predict == y_test[:1000])
-    print("Prediction accuracy using SVM for ", fileName1)
-    print("%d out of %d predictions correct" % (correct, len(y_predict)))
-    
-    # compute prediction accuracy using SVM for file1
-    svm_clf = svmPlus.svmPlusOpt(XStar_train, yStar_train, XStar=None, C=100, kernel="rbf",
-                                 kernelParam=.00001)  # standard SVM
-    y_predict = svmPlus.predict(XStar_test[:1000], svm_clf)
-    correct = np.sum(y_predict == yStar_test[:1000])
-    print("Prediction accuracy using SVM for ", fileName2)
-    print("%d out of %d predictions correct" % (correct, len(y_predict)))
+    # record the results in a file
+    cv = StratifiedKFold(n_splits=5)
+    folds = [[train_index, test_index] for train_index, test_index in cv.split(X, y)]
+    predAcc = []
+    index = []
 
-    # compute prediction accuracy using SVM on finger desc file
-    svm_clf = svmPlus.svmPlusOpt(XFD_train, yFD_train, XStar=None, C=100, kernel="rbf",
-                                 kernelParam=.00001)  # standard SVM
-    y_predict = svmPlus.predict(XFD_test[:1000], svm_clf)
-    correct = np.sum(y_predict == yFD_test[:1000])
-    print("Prediction accuracy using SVM for ", fingDescFile)
-    print("%d out of %d predictions correct" % (correct, len(y_predict)))
-    '''
-    C = 1
-    gamma = .0001
-    # compute prediction accuracy using SVM+ on file1, and file2 as a priv-info
-    clf = svmPlus.svmPlusOpt(X_train, y_train, XStar=XStar_train, C = C, kernel="rbf",
+    ofile = open('paramGridResults/' + "SVMPlus" + MACCSFile[18:-4], "a")
+    ofile.write("Size of the train set" + str(X.shape[0]) + "\n")
+    ofile.write("Size of the test set" + str(X_test.shape[0]) + "\n")
+
+    for i in range(len(paramC)):
+        for j in range(len(paramGamma)):
+            accuracy = 0
+            for fold in folds:
+                '''
+                # compute prediction accuracy using SVM+ on MACCSFile, and topoFile as a priv-info
+                clf = svmPlus.svmPlusOpt(X_train, y_train, XStar=XStar_train, C = paramC[i], kernel="rbf",
+                                         kernelParam=0.1, kernelStar="rbf", kernelStarParam=0.001,
+                                         gamma = paramGamma[j])
+                y_predict = svmPlus.predict(X_test, clf)
+                correct = np.sum(y_predict == y_test)
+                print("Prediction accuracy using SVM+ for ", MACCSFile, topoFile)
+                #print("%d out of %d predictions correct" % (correct, len(y_predict)))
+                predAcc = round(correct / len(y_predict), 3)
+                print("param C = %f, gamma = %f, pred accuracy = %f \n" % (C, gamma, predAcc))
+                '''
+                # compute prediction accuracy using SVM+ on MACCSFile, and fingDescFile as a priv-info
+                clf = svmPlus.svmPlusOpt(X[fold[0]], y[fold[0]], XStar=XFD[fold[0]], C = paramC[i], kernel="rbf",
+                                         kernelParam=0.1, kernelStar="rbf", kernelStarParam=0.001,
+                                         gamma = paramGamma[j])
+                y_predict = svmPlus.predict(X[fold[1]], clf)
+                correct = np.sum(y_predict == y[fold[1]])
+                accuracy = accuracy + (correct / len(y_predict))
+
+            print(i, j)
+            accuracy = accuracy / 5
+            predAcc.append(accuracy)
+            index.append([i, j])
+            ofile.write("param C = %f, gamma = %f, mean pred accuracy = %f \n" %
+                        (paramC[i], paramGamma[j], accuracy))
+
+    selectedIndex = np.argmax(predAcc)
+    C = paramC[index[selectedIndex][0]]
+    gamma = paramGamma[index[selectedIndex][1]]
+
+    # For optimal parameters
+    clf = svmPlus.svmPlusOpt(X, y, XStar=XFD, C=C, kernel="rbf",
                              kernelParam=0.1, kernelStar="rbf", kernelStarParam=0.001,
-                             gamma = gamma)
+                             gamma=gamma)
     y_predict = svmPlus.predict(X_test, clf)
     correct = np.sum(y_predict == y_test)
-    print("Prediction accuracy using SVM+ for ", fileName1, fileName2)
-    #print("%d out of %d predictions correct" % (correct, len(y_predict)))
+    # print("Prediction accuracy using SVM+ for ", MACCSFile, fingDescFile)
+    # print("%d out of %d predictions correct" % (correct, len(y_predict)))
     predAcc = round(correct / len(y_predict), 3)
-    print("param C = %f, gamma = %f, pred accuracy = %f \n" % (C, gamma, predAcc))
+    # print("param C = %f, gamma = %f, pred accuracy = %f \n" % (C, gamma, predAcc))
+    #print("param C = %f, gamma = %f, AUC = %f \n" % (C, gamma, predAcc))
+    ofile.write("param C = %f, gamma = %f, AUC = %f \n" % (C, gamma, predAcc))
 
-    # compute prediction accuracy using SVM+ on file1, and file2 as a priv-info
-    clf = svmPlus.svmPlusOpt(X_train, y_train, XStar=XFD_train, C = C, kernel="rbf",
-                             kernelParam=0.1, kernelStar="rbf", kernelStarParam=0.001,
-                             gamma = gamma)
-    y_predict = svmPlus.predict(X_test, clf)
-    correct = np.sum(y_predict == y_test)
-    print("Prediction accuracy using SVM+ for ", fileName1, fingDescFile)
-    #print("%d out of %d predictions correct" % (correct, len(y_predict)))
-    predAcc = round(correct / len(y_predict), 3)
-    print("param C = %f, gamma = %f, pred accuracy = %f \n" % (C, gamma, predAcc))
+    ofile.close()
+
 
 
 # Print dimensions of the various descriptor files
 def readDetailsDescriptorFiles():
-    for fileName in listFileName:
+    for fileName in MACCSFiles:
         X, y = loadDataset(fileName)
         print("Details of the file", fileName)
         print(X.shape)
         getIndicesWithMissingValues(fileName) #print info about missing values
-
+    for fileName in topologicalFiles:
+        X, y = loadDataset(fileName)
+        print("Details of the file", fileName)
+        print(X.shape)
+        getIndicesWithMissingValues(fileName) #print info about missing values
     for fileName in fingerPrintFiles:
         X, y = csr.readCSRFile(fileName)
         print("Details of the file", fileName)
@@ -234,17 +244,18 @@ def readDetailsDescriptorFiles():
 
 
 #global Variables: descriptor file names
-listFileName = ["DescriptorDataset/sr-mmp_nosalt.sdf.std_nodupl_class.sdf_MACCS_166bit.csv",
-    "DescriptorDataset/sr-mmp_nosalt.sdf.std_nodupl_class.sdf_topological.csv",
-    "DescriptorDataset/smiles_cas_N6512.sdf.std_class.sdf_MACCS_166bit.csv",
-    "DescriptorDataset/smiles_cas_N6512.sdf.std_class.sdf_topological.csv",
-    "DescriptorDataset/bursi_nosalts_molsign.sdf.txt_MACCS_166bit.csv",
-    "DescriptorDataset/bursi_nosalts_molsign.sdf.txt_topological.csv" ]
+MACCSFiles = ["DescriptorDataset/sr-mmp_nosalt.sdf.std_nodupl_class.sdf_MACCS_166bit.csv",
+              "DescriptorDataset/smiles_cas_N6512.sdf.std_class.sdf_MACCS_166bit.csv",
+              "DescriptorDataset/bursi_nosalts_molsign.sdf.txt_MACCS_166bit.csv"]
+
+#global Variables: descriptor file names
+topologicalFiles = ["DescriptorDataset/sr-mmp_nosalt.sdf.std_nodupl_class.sdf_topological.csv",
+                    "DescriptorDataset/smiles_cas_N6512.sdf.std_class.sdf_topological.csv",
+                    "DescriptorDataset/bursi_nosalts_molsign.sdf.txt_topological.csv"]
 
 fingerPrintFiles = ["DescriptorDataset/sr-mmp_nosalt.sdf.std_nodupl_class.sdf_SVMLIGHT_Morgan_unhashed_radius_3.csv",
                     "DescriptorDataset/smiles_cas_N6512.sdf.std_class.sdf_SVMLIGHT_Morgan_unhashed_radius_3.csv",
-                    "DescriptorDataset/bursi_nosalts_molsign.sdf.txt_SVMLIGHT_Morgan_unhashed_radius_3.csv"
-                    ]
+                    "DescriptorDataset/bursi_nosalts_molsign.sdf.txt_SVMLIGHT_Morgan_unhashed_radius_3.csv"]
 
 
 #run SVM for finger print descriptor file
@@ -340,13 +351,12 @@ def gridSearchTopological(fileName):
 
 if __name__ == "__main__":
     #readDetailsDescriptorFiles()
-    #for fileName in listFileName:
-    #    runSVMSigDescFile(fileName)
+    #for fileName in MACCSFiles:
+    #    gridSearchWithCV(fileName)
     #svmOnFingDescFile(fingerPrintFiles[0])
-    gridSearchWithCV(listFileName[0])
     #gridSearchWithCV(fingerPrintFiles[0])
-    #compareSVMAndSVMPlus(listFileName[0], listFileName[1], fingerPrintFiles[0])
-    #compareSVMAndSVMPlus(listFileName[2], listFileName[3], fingerPrintFiles[1])
-    #compareSVMAndSVMPlus(listFileName[4], listFileName[5], fingerPrintFiles[2])
+    gridSearchSVMPlus(MACCSFiles[0], topologicalFiles[0], fingerPrintFiles[0])
+    #compareSVMAndSVMPlus(MACCSFiles[1], topologicalFiles[1], fingerPrintFiles[1])
+    #compareSVMAndSVMPlus(MACCSFiles[2], topologicalFiles[2], fingerPrintFiles[2])
 
 
